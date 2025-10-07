34f4370d971d3b165ce4a852701cce8534668c75
Leon Windhorst <leon@windhorst.dev>
2025-10-07 15:28:14 +0200

    Add EXCLUDE_FROM_ALL to individual projects

--- projects/sandbox/src/main.cpp
+++ projects/sandbox/src/main.cpp
@@ -1,6 +1,11 @@
-#include <cstdio>
+#include "VkBootstrap.h"
+#include "volk.h"
 
 #include "fmt/core.h"
+
+vkb::Instance instance;
+vkb::Device device;
+vkb::Swapchain swapchain;
 
 int
 main(int argc, char **argv)
