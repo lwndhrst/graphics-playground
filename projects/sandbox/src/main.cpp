78c0d49c9b2d84ee732ea28a77ee19baeb05f409
Leon Windhorst <leon@windhorst.dev>
2025-10-15 15:32:58 +0200

    Generate much larger mesh

--- projects/sandbox/src/main.cpp
+++ projects/sandbox/src/main.cpp
@@ -302,8 +302,8 @@ create_pipeline()
     rasterizer.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
     rasterizer.depthClampEnable = VK_FALSE;
     rasterizer.rasterizerDiscardEnable = VK_FALSE;
-    // rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
-    rasterizer.polygonMode = VK_POLYGON_MODE_LINE;
+    rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
+    // rasterizer.polygonMode = VK_POLYGON_MODE_LINE;
     rasterizer.lineWidth = 1.0f;
     rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
     // rasterizer.cullMode = VK_CULL_MODE_NONE;
